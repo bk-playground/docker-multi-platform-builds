f10406c3c236b197b555bb8c528afa76066e81d4
Mark Wolfe <mark.wolfe@buildkite.com>
2025-02-14 13:41:38 +1100

    fix: adjust installation path

--- .buildkite/scripts/install-go.sh
+++ .buildkite/scripts/install-go.sh
@@ -4,8 +4,7 @@ printf "Checking latest Go version...\n";
 LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)";
 LATEST_GO_DOWNLOAD_URL="https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 
-printf "cd to home (%s) directory \n" $USER
-cd $HOME || exit
+cd /usr/local || exit
 
 printf "Downloading %s\n\n" ${LATEST_GO_DOWNLOAD_URL};
 curl -OJ -L --progress-bar $LATEST_GO_DOWNLOAD_URL
@@ -13,5 +12,8 @@ curl -OJ -L --progress-bar $LATEST_GO_DOWNLOAD_URL
 printf "Extracting file...\n"
 tar -xf "${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 
+printf "Setting up environment variables...\n"
+export PATH="/usr/local/go/bin:$PATH"
+
 printf "You are ready to Go!";
-$HOME/go/bin/go version
+go version
